84929609fb8f5f8b93bd71f5a7f34383b7b6b614
chezzzer <46093784+chezzzer@users.noreply.github.com>
2023-03-02 18:07:27 +1300

    up

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -2,9 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import spotify, { refreshToken } from "../../inc/spotify";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
     if (req.method !== "POST") return res.status(400).end("Method not allowed");
-    if (!req.body.search) return res.status(400).end("Search parameter not found.");
+    if (!req.body.search)
+        return res.status(400).end("Search parameter not found.");
 
     await refreshToken();
 
@@ -13,7 +17,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             market: "NZ",
             limit: 30,
         });
-        res.status(200).json(results.body.tracks?.items);
+        const items = results.body.tracks?.items.filter((track) => {
+            if (!track.explicit) {
+                return true;
+            }
+        });
+
+        res.status(200).json(items);
     } catch (e) {
         console.error("Error fetching search list.");
         res.status(500).end("Error fetching search list.");
